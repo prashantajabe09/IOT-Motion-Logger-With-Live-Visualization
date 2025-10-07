240f5e8032e901884df9ef0499f078755616b26f
Prashant Ajabe <prashantajabe09@gmail.com>
2025-10-07 22:43:59 +0530

    added the esp8266 sketch

--- ESP8266_with_STM32/ESP8266_with_STM32.ino
+++ ESP8266_with_STM32/ESP8266_with_STM32.ino
@@ -2,32 +2,51 @@
 
 const char* ssid = "TP-Link_5DD0";
 const char* password = "76144493";
-const char* host = "192.168.1.101";  // PC running Hercules
+const char* host = "192.168.1.101";  // Server IP
 const int port = 10013;
 
 WiFiClient client;
 
+void connectToServer() {
+  while (!client.connect(host, port)) {
+    Serial.println("Connecting to TCP server...");
+    delay(1000);
+  }
+  Serial.println("Connected to TCP server");
+}
+
 void setup() {
-  Serial.begin(115200);   // UART to STM32
+  Serial.begin(115200);
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.println("Connecting to WiFi...");
   }
-
   Serial.println("Connected to WiFi");
 
-  if (client.connect(host, port)) {
-    Serial.println("Connected to TCP server");
-  } else {
-    Serial.println("Connection to TCP server failed");
-  }
+  connectToServer();
 }
 
 void loop() {
-  if (client.connected() && Serial.available()) {
-    String data = Serial.readStringUntil('\n');  // read STM32 line
-    client.println(data);                        // send to Hercules
+  // if connection lost → reconnect
+  if (!client.connected()) {
+    Serial.println("Server disconnected, reconnecting...");
+    client.stop();
+    delay(1000);
+    connectToServer();
+  }
+
+  // if STM32 sent something
+  if (Serial.available()) {
+    String data = Serial.readStringUntil('\n');  // careful with blocking
+    client.print(data + "\n");                   // send clean newline
+    Serial.println("Sent: " + data);
+  }
+
+  // if server sends data back, read it
+  while (client.available()) {
+    String line = client.readStringUntil('\n');
+    Serial.println("From server: " + line);
   }
 }
